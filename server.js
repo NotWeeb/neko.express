dc3a5e82d40bac54e92c182c90a6313692b1946d
Ken <kyrolites@gmail.com>
2018-04-12 00:43:05 +0800

    zz

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-//const http = require('http')
+const http = require('http')
 const path = require('path')
 const https = require('https')
 const key = fs.readFileSync('./key.pem')
@@ -10,15 +10,11 @@ const forceSsl = require('express-force-ssl');
 const express = require('express');
 const app = express();
 
-//const httpServer = http.createServer(app);
-const httpsServer = https.createServer({key, cert}, app);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({key: key, cert: cert}, app);
 
-//httpServer.listen(80);
+httpServer.listen(80);
 httpsServer.listen(443);
-
-app.use('/.well-known', express.static(path.join(__dirname, '.well-known')))
-
-app.use(forceSsl);
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(__dirname + '/index.html')
