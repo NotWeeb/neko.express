cc1a9182d7b852bbc93b58aa42bc5e0d200e3cb8
Ken <kyrolites@gmail.com>
2018-04-12 00:12:18 +0800

    zz

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
-const https = require('https')
-const forceSsl = require('express-force-ssl');
 
 
 const express = require('express');
 const app = express();
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(app);
 
 httpServer.listen(80);
-httpsServer.listen(443);
-
-app.use(forceSsl);
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(__dirname + '/index.html')
